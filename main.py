ff2afe17f450855347709a3a2a81d4f1f185b22a
muhammed-abdul-muid <muhammed.a.muid@houseofedtech.in>
2026-03-31 11:33:37 +0530

    updated data flow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 '''
 read packages.json (packageName: versionNumber)
-hit https://api.osv.dev/v1/querybatch with content of packages.json 
-if vulns returned -> slack alert
-if nothing -> silence
+hit https://api.osv.dev/v1/querybatch with content of packages.json get vuln ids
+fetch each id -> get full details
+derive severity
+find safe version (first version before affected range)
+slack: package, severity, affected versions, recommended action
 '''
